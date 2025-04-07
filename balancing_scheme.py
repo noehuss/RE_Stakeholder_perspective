0fab1d841db9f8665598620b8781f265ef4a685c
Noé <noehusser@gmail.com>
2025-04-07 22:17:15 +0200

    code refraction offering strategy and plot cdf

---
+++ balancing_scheme.py
@@ -0,0 +1,25 @@
+from offering_strategy import OnePriceScheme, TwoPricesScheme
+import pandas as pd
+from data_creation import scenario_generator
+import matplotlib.pyplot as plt
+
+df_scenario = scenario_generator()
+
+P_nom = 500
+nb_hours = 24
+
+OnePrice = OnePriceScheme(T=nb_hours, scenarios=df_scenario[0:200], Pnom=P_nom)
+OnePrice.solve_model()
+profit_one_price = OnePrice.get_profit_distribution()
+OnePrice.model.p_DA.pprint()
+
+TwoPrice = TwoPricesScheme(T=nb_hours, scenarios=df_scenario[0:200], Pnom=P_nom)
+TwoPrice.solve_model()
+profit_two_price = TwoPrice.get_profit_distribution()
+TwoPrice.model.p_DA.pprint()
+
+# Plot one price and two price comulative distribution profit
+plt.hist(cumulative=True, x=profit_one_price[0], density=True, bins=100, alpha=0.5, label='One price')
+plt.hist(cumulative=True, x=profit_two_price[0], density=True, bins=100, alpha=0.5, label='Two prices')
+plt.legend()
+plt.show()
--- offering_strategy.py
+++ offering_strategy.py
@@ -13,7 +13,7 @@ class OfferingStrategy():
 
     def indexes(self):
         self.model.hours = RangeSet(0, self.T-1) # 1 to T
-        self.model.scenarios = RangeSet(0, self.nb_scenarios-1)
+        self.model.scenarios = Set(initialize=self.scenarios.index)
 
     def parameters(self):
         self.model.price = Param(self.model.scenarios, self.model.hours, within=Reals, initialize= lambda model, s, h: self.scenarios.loc[s,"Price"][h], name="price")
@@ -22,14 +22,9 @@ class OfferingStrategy():
 
     def variables(self):
         self.model.p_DA = Var(self.model.hours, bounds = (0, self.Pnom), name="pDA")
-        # self.model.delta_up = Var(self.model.scenarios, self.model.hours, domain = NonNegativeReals)
-        # self.model.delta_down = Var(self.model.scenarios, self.model.hours, domain = NonNegativeReals)
         self.model.delta = Var(self.model.scenarios, self.model.hours, domain = Reals, name="Delta")
     
     def constraints(self):
-        # def equality_delta (model, s , h):
-        #     return model.delta[(s,h)] == model.delta_up[(s,h)] - model.delta_down[(s,h)]
-        # self.model.def_delta = Constraint (self.model.scenarios, self.model.hours, rule = equality_delta)
         def rule_imbalance(model, s, h):
             return model.delta[(s,h)] == model.wind[(s,h)]*self.Pnom - model.p_DA[h]
         self.model.imbalance = Constraint (self.model.scenarios, self.model.hours, rule = rule_imbalance) 
@@ -51,28 +46,31 @@ class OfferingStrategy():
         solution = solver.solve(self.model, tee=True)
         #self.model.write("model.lp")
 
-    
+    def _profit(self, s:int, t:int):
+        pass
 
-class OnePriceScheme(OfferingStrategy):
     def objective_function(self):
-        self.model.objective = Objective(expr = (1/self.nb_scenarios)*sum(sum(self.model.price[(s,t)]*self.model.p_DA[(t)]
-                                                        +self.model.price[(s,t)]*self.model.delta[(s,t)]*
-                                                        (0.85*(1-self.model.sys_condition[(s,t)])
-                                                        +1.25*self.model.sys_condition[(s,t)])
+        self.model.objective = Objective(expr = (1/self.nb_scenarios)*sum(sum( self._profit(s,t)
                                                          for s in self.model.scenarios ) for t in self.model.hours), 
                                         sense=maximize)
 
-    def get_profit_distribution(self) -> pd.DataFrame:
+    def get_profit_distribution(self, plot:bool=False) -> pd.DataFrame:
         profit = pd.DataFrame(index=self.model.scenarios, columns=[t for t in self.model.hours])
         for s in self.model.scenarios:
             for t in self.model.hours:
-                profit.loc[s, t] = value(self.model.price[(s,t)]*(self.model.p_DA[t]+self.model.delta[(s,t)]*(
-                                    1.25*self.model.sys_condition[(s,t)]+0.85*(1-self.model.sys_condition[(s,t)]))))
-        profit[0].sort_values(axis=0, ascending=True).plot.bar(y=profit.index)
-        plt.show()
-        profit[0].hist(cumulative=True, density=1, bins=100, grid=False)
-        plt.show()
+                profit.loc[s, t] = value(self._profit(s,t))
+        if plot:
+            profit[0].hist(cumulative=True, density=1, bins=100, grid=False)
+            plt.show()
         return profit
+    
+
+class OnePriceScheme(OfferingStrategy):
+    def _profit(self, s:int, t:int):
+        return self.model.price[(s,t)]*(self.model.p_DA[(t)]+self.model.delta[(s,t)]*
+                                                        (0.85*(1-self.model.sys_condition[(s,t)])
+                                                        +1.25*self.model.sys_condition[(s,t)]))
+
 
 class TwoPricesScheme(OfferingStrategy):
         def variables(self):
@@ -86,23 +84,7 @@ class TwoPricesScheme(OfferingStrategy):
                 return model.delta[(s,h)] == model.delta_up[(s,h)] - model.delta_down[(s,h)]
             self.model.def_delta = Constraint (self.model.scenarios, self.model.hours, rule = equality_delta)
 
-        def objective_function(self):
-            self.model.objective = Objective(expr = (1/self.nb_scenarios)*sum(sum(self.model.price[(s,t)]*self.model.p_DA[(t)]
-                                                        +self.model.price[(s,t)]*
-                                                        (self.model.sys_condition[(s,t)]*(self.model.delta_up[(s,t)]-1.25*self.model.delta_down[(s,t)])
-                                                        +(1-self.model.sys_condition[(s,t)])*(0.85*self.model.delta_up[(s,t)]-self.model.delta_down[(s,t)])) 
-                                                        for s in self.model.scenarios ) for t in self.model.hours), 
-                                        sense=maximize)
-            
-        def get_profit_distribution(self) -> pd.DataFrame:
-            profit = pd.DataFrame(index=self.model.scenarios, columns=[t for t in self.model.hours])
-            for s in self.model.scenarios:
-                for t in self.model.hours:
-                    profit.loc[s, t] = value(self.model.price[(s,t)]*self.model.p_DA[(t)]+self.model.price[(s,t)]*
+        def _profit(self, s:int , t:int):
+            return self.model.price[(s,t)]*(self.model.p_DA[(t)]+
                                             (self.model.sys_condition[(s,t)]*(self.model.delta_up[(s,t)]-1.25*self.model.delta_down[(s,t)])
-                                            +(1-self.model.sys_condition[(s,t)])*(0.85*self.model.delta_up[(s,t)]-self.model.delta_down[(s,t)])))
-            profit[0].sort_values(axis=0, ascending=True).plot.bar(y=profit.index)
-            plt.show()
-            profit[0].hist(cumulative=True, density=1, bins=100, grid=False)
-            plt.show()
-            return profit
+                                            +(1-self.model.sys_condition[(s,t)])*(0.85*self.model.delta_up[(s,t)]-self.model.delta_down[(s,t)]))) 
