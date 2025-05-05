40a2260f522ab4a39c45f6f0e705d7e6875a4127
Noé <noehusser@gmail.com>
2025-05-05 17:34:24 +0200

    task2

--- P90_requirement.py
+++ P90_requirement.py
@@ -4,58 +4,121 @@ import matplotlib.pyplot as plt
 from pyomo.contrib.iis import write_iis
 import numpy as np
 import param
+import data.generation_scen_task2 as g
 
 
 class OptimalReserveCapacity():
-    def __init__(T, scenarios:list[list[float]]):
+    def __init__(self, scenarios:list[list[float]],T=24, P=0.9):
+        """
+        scenarios: list[list[float]] \n
+        T: number of hours, default: T=24 \n
+        P: P requirement, default: P90, P=0.9 \n
+        """
         self.model = ConcreteModel()
         self.T = T
         self.scenarios = scenarios
         self.nb_scenarios = len(scenarios)
+        self.P = P
 
-    def indexes():
+    def indexes(self):
         self.model.scenarios = RangeSet(0, self.nb_scenarios-1)
         self.model.hours = RangeSet(0, self.T-1) # 1 to T
         
+    def parameters(self):
+        self.model.F_up = Param(self.model.scenarios, self.model.hours, initialize= lambda model, s, h: self.scenarios[s][h], name="F_up" )
+
+    def variables(self):
+        self.model.c_up = Var(within=NonNegativeReals)
+
+    def constraints(self):
+        pass 
+
+    def objective_function(self):
+        self.model.objective = Objective(expr = self.model.c_up, sense = maximize)
+
+    def solve_model(self):
+        self.indexes()
+        self.parameters()
+        self.variables()
+        self.constraints()
+        self.objective_function()
+        # Dual
+        self.model.dual = Suffix(direction=Suffix.IMPORT)
+        # Create a solver
+        solver = SolverFactory("gurobi", solver_io="python")  # Make sure Gurobi is installed and properly configured
+        # Solve the model
+        solution = solver.solve(self.model, tee=True)
+        #self.model.write("model.lp")
+
+    def return_c_up(self):
+        return value(self.model.c_up)
+    
+
+class CVaR(OptimalReserveCapacity):    
+    def variables(self):
+        super().variables()
+        self.model.y = Var(self.model.scenarios, self.model.hours, bounds=(0,1))
+        self.model.beta = Var(within = NegativeReals)
+        self.model.zeta = Var(self.model.scenarios, self.model.hours, within = Reals)
         
-    def parameters():
-        self.model.F_up = Param(self.modeel.scenarios, )
-
-    def variables():
-        self.model.c_up = Var(bounds=())
-
-
-    def constraints():
-
-    def objective_function():
-
-    def solve_model():
-
-
-class CVaR(OptimalReserveCapacity):
-    def __init__():
-        super.__init__()
-    
-    def indexes():
-        super.indexes()
-    
-    def parameters():
-        super.parameters()
-    
-    def variables():
-        super.variables()
+    def constraints(self):
+        def rule_c_up(model, s, h):
+            return model.c_up - model.F_up[s,h] <= model.zeta[s,h]  
+        self.model.constraint_c_up = Constraint(self.model.scenarios, self.model.hours, rule = rule_c_up)      
+        
+        def rule_zeta(model, s,h):
+            return model.beta <= model.zeta[s,h]
+        self.model.constraint_zeta = Constraint(self.model.scenarios, self.model.hours, rule = rule_zeta) 
+        
+        def rule(model):
+            return (sum(sum(model.zeta[s,h] for s in self.model.scenarios) for h in self.model.hours))/(self.T*self.nb_scenarios)<=self.model.beta*self.P
+        self.model.constraint = Constraint(rule = rule)
 
 
 class AlsoX(OptimalReserveCapacity):
-    def __init__():
-        super.__init__()
-    
-    def indexes():
-        super.indexes()
-    
+    def __init__(self, scenarios, T=24, P=0.9):
+        super().__init__(scenarios, T, P)
+        self.M = 1000
+        self.q = (1-self.P)*self.nb_scenarios*self.T
+
     def variables(self):
         super().variables()
+        self.model.y = Var(self.model.scenarios, self.model.hours, within = Binary)
         
-    
-    
+    def constraints(self):
+        def rule_c_up(model, s, h):
+                return model.c_up - model.F_up[s,h] <= self.model.y[s, h]*self.M
+        self.model.constraint_c_up = Constraint(self.model.scenarios, self.model.hours, rule=rule_c_up) 
         
+        def rule_q(model):
+            return  sum(sum(self.model.y[s,h]for s in self.model.scenarios) for h in self.model.hours)<= self.q
+        self.model.constraint_q = Constraint(rule=rule_q)
+
+
+def plot_overbid(c_up, scenarios):
+    freq = []
+    for scenario in scenarios:
+        count_overbid = sum(1 for i in scenario if i < c_up)
+        freq.append(count_overbid/len(scenario))
+    plt.hist(freq, density=True, bins=60)
+    plt.axvline(0.1, linestyle='--', color='green', label='P90 requirement')
+    plt.axvline(np.mean(freq), linestyle='--', color='red', label=f'mean = {np.mean(freq)*100:.2f} %')
+    plt.legend()
+    plt.show()
+
+
+scenarios = g.generate_scenarios()        
+
+alsoX = AlsoX(scenarios=scenarios[:100])
+alsoX.solve_model()
+
+cvar = CVaR(scenarios=scenarios[:100])
+cvar.solve_model()
+
+print(alsoX.return_c_up())        
+print(cvar.return_c_up())
+
+plot_overbid(alsoX.return_c_up(), scenarios[:100])
+plot_overbid(alsoX.return_c_up(), scenarios[100:300])
+plot_overbid(cvar.return_c_up(), scenarios[:100])
+plot_overbid(cvar.return_c_up(), scenarios[100:300])
---
+++ data/generation_scen_task2.py
@@ -0,0 +1,24 @@
+import random as rd
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def generate_scenarios() -> list[list[float]]:
+    rd.seed(0)
+    minutes =  [i for i in range(0,60)]
+    loads = [ [rd.randint(220,600)] for i in range(300)]
+    for k in range(300):
+        for i in range(1,60):
+            random = rd.randint(max(220-loads[k][i-1],-35),min(600-loads[k][i-1],35))
+            loads[k].append(loads[k][i-1]+random)
+    return loads
+
+def plot_distrib(loads_list):
+    distrib = []
+    for loads in loads_list:
+        distrib += loads
+    print(distrib)
+    plt.hist(distrib, bins=50, density=True)
+    plt.show()
+
+plot_distrib(generate_scenarios()[:100])
